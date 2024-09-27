6436590909a909d4a2eeb72c5139e0a995b82a30
Duy Hung <hungtd.sol@gmail.com>
2024-09-28 00:27:43 +0700

    Wording

--- app/src/main/java/com/example/viewapptest/MainActivity.kt
+++ app/src/main/java/com/example/viewapptest/MainActivity.kt
@@ -16,7 +16,7 @@ import androidx.core.view.WindowInsetsCompat
  * - Run the app, manually grant notification permission in app info
  * (The app will load and show an interstitial ad, also start a foreground service). Only when the app
  * has shown a test notification, move to the next step
- * - Remove the app from recent apps (The app should keep running because of the service)
+ * - Close and remove the app from recent apps (The app should keep running because of the service)
  * - Now open the app again, there will be 2 bugs:
  *     + Pre-cached ads will be error, in this example, interstitial ads will show a transparent
  *     activity overlay. You have to press back to remove it. App open ads is the same as I tested.
